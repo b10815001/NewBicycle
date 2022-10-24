e1d52c26e44bd8a8dbaddd67551aa2ec2ec29fc1
LittleLemon666 <damoncho510@gmail.com>
2022-10-24 09:43:28 +0800

    Chenggongling 4 cams

--- Assets/Scripts/Anchor.cs
+++ Assets/Scripts/Anchor.cs
@@ -24,6 +24,15 @@ public class Anchor : MonoBehaviour
     public void displayAnchors()
     {
         GameObject anchor_manager = new GameObject("Anchor Manager");
+        GameObject anchor_manager_mainroad = new GameObject("Anchor Manager Mainroad");
+        GameObject anchor_manager_branch = new GameObject("Anchor Manager Branch");
+        GameObject anchor_manager_house = new GameObject("Anchor Manager House");
+        GameObject anchor_manager_other = new GameObject("Anchor Manager Other");
+        anchor_manager_mainroad.transform.parent = anchor_manager.transform;
+        anchor_manager_branch.transform.parent = anchor_manager.transform;
+        anchor_manager_house.transform.parent = anchor_manager.transform;
+        anchor_manager_other.transform.parent = anchor_manager.transform;
+
         using (StreamReader sr = new StreamReader($"Assets/{data_dir}/{file_name}"))
         {
             string[] lines = sr.ReadToEnd().Split('\n');
@@ -31,6 +40,8 @@ public class Anchor : MonoBehaviour
             for (int i = 0; i < lines.Length; i++)
             {
                 string[] inputs = lines[i].Split(' ');
+                if (inputs.Length < 3)
+                    continue;
                 GameObject ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 ball.transform.position = new Vector3(float.Parse(inputs[0]), float.Parse(inputs[1]), float.Parse(inputs[2]));
                 var material = new Material(ball.GetComponent<Renderer>().sharedMaterial);
@@ -39,23 +50,33 @@ public class Anchor : MonoBehaviour
                     case "Mainroad":
                         ball.name = $"{inputs[3]} {main_index++}";
                         material.color = Color.red;
+                        ball.GetComponent<Renderer>().sharedMaterial = material;
+                        ball.transform.parent = anchor_manager_mainroad.transform;
                         break;
                     case "Branch":
                         ball.name = inputs[3];
                         material.color = Color.blue;
+                        ball.GetComponent<Renderer>().sharedMaterial = material;
+                        ball.transform.parent = anchor_manager_branch.transform;
                         break;
                     case "House":
                         ball.name = inputs[3];
                         material.color = Color.yellow;
+                        ball.GetComponent<Renderer>().sharedMaterial = material;
+                        ball.transform.parent = anchor_manager_house.transform;
                         break;
                     case "Other":
                         ball.name = inputs[3];
                         material.color = Color.grey;
+                        ball.GetComponent<Renderer>().sharedMaterial = material;
+                        ball.transform.parent = anchor_manager_other.transform;
                         break;
                 }
-                ball.GetComponent<Renderer>().sharedMaterial = material;
-                ball.transform.parent = anchor_manager.transform;
             }
         }
+
+        anchor_manager_branch.SetActive(false);
+        anchor_manager_house.SetActive(false);
+        anchor_manager_other.SetActive(false);
     }
 }
