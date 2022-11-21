60269cf898e208c353155d4d839e837088ca3deb
b10815001 <94827066+b10815001@users.noreply.github.com>
2022-11-21 12:32:58 +0800

    camera
    road1

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -9,10 +9,19 @@ public class PathFollower : MonoBehaviour
     private float covered_distance = 0;
     public float slope = 0;
 
-    private float speed = 0.1f;
-    private Vector3 camera_height = Vector3.up * 2;
+    private float speed = 0.5f;
+    private Vector3 camera_height = Vector3.up * 3;
 
     public bool procedural = false;
+
+    private int current_num = 0;
+    private int current_control = 0;
+    private float current_seg = 0;
+    private List<int> road_num = new List<int>() { 1, 4, 2, 3, 1 };
+    private List<Vector2> road_data = new List<Vector2>() { new Vector2(0, 2), new Vector2(3, 1), new Vector2(1, 28), new Vector2(1, 4), new Vector2(48, 0) };
+    private int road_seg = 0;
+    private int control_seg = 0;
+    private int seg = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +37,31 @@ public class PathFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (true || Input.GetKey(KeyCode.Space)) arclength();
+        if (Input.GetKey(KeyCode.Space)) arclength();
+        //transform.position = movement();
+    }
+
+    private Vector3 movement()
+    {
+        int total_control = 0;
+        int id = 0;
+        for (; id < road_num.Count; id++)
+        {
+            total_control += (int)Mathf.Abs(road_data[id][1] - road_data[id][0]);
+            if (current_control < total_control)
+            {
+                road = GameObject.Find("Road" + (road_num[id]).ToString());
+                break;
+            }
+        }
+
+        int grade = (int)((road_data[id][0] - road_data[id][1]) / Mathf.Abs(road_data[id][0] - road_data[id][1]));
+        int this_node = (int)road_data[id][1] + grade * (total_control - current_control);
+        RoadArchitect.SplineN node = road.GetComponent<RoadArchitect.Road>().spline.nodes[this_node];
+        int that_node = (int)road_data[id][1] + grade * (total_control - current_control - 1);
+        RoadArchitect.SplineN node2 = road.GetComponent<RoadArchitect.Road>().spline.nodes[that_node];
+
+        return Vector3.zero;
     }
 
     private void arclength()
@@ -99,9 +132,13 @@ public class PathFollower : MonoBehaviour
 
                 if (distance == 0)
                 {
-                    transform.position = Vector3.up * 2 + nodes[current_node].transform.position + (nodes[next_node].transform.position - nodes[current_node].transform.position).normalized * covered_distance;
+                    Vector3 linear_pos = nodes[current_node].pos + (nodes[next_node].pos - nodes[current_node].pos).normalized * covered_distance;
+                    transform.position = camera_height + road.GetComponent<RoadArchitect.Road>().spline.GetSplineValue(road.GetComponent<RoadArchitect.Road>().spline.GetClosestParam(linear_pos, false, false));
                     Quaternion last_rotation = transform.rotation;
-                    transform.LookAt(Vector3.up * 2 + nodes[next_node].transform.position, Vector3.up);
+
+                    int next_next_node = (next_node + 1 < nodes.Count) ? (next_node + 1) : 0;
+                    Vector3 look_at = nodes[next_node].pos + (nodes[next_next_node].pos - nodes[next_node].pos).normalized * covered_distance;
+                    transform.LookAt(camera_height + road.GetComponent<RoadArchitect.Road>().spline.GetSplineValue(road.GetComponent<RoadArchitect.Road>().spline.GetClosestParam(look_at, false, false)), Vector3.up);
                     transform.rotation = Quaternion.Lerp(last_rotation, transform.rotation, 0.01f);
                 }
 
